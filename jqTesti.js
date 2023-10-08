30f953f1a2150fa4145819a8fc76ca9b38351388
RenatHuseynzadeh <124487433+RenatHuseynzadeh@users.noreply.github.com>
2023-10-08 14:09:05 +0300

    Add files via upload

---
+++ jqTesti.js
@@ -0,0 +1,60 @@
+
+// 1
+// к примеру text,val,html
+
+// 2
+// что бы взять текст из еллемента
+
+// 3
+// что бы взять теги и значения из html
+
+// 4
+// что бы взять то что написано внутри инпута
+
+// 5
+//  console.log($('.text').text());
+
+// 6
+//  console.log($('.text').html());
+
+// 7
+// console.log($('.text').val());
+
+// 8
+//  $('.text').text('NewText');
+
+// 9
+// $('.text').html('<p>NewText</p>');
+
+// 10
+// $('.text').val('NewText')
+
+// 11
+// он может взять и задать атрибут эллементу
+
+// 12,13
+// $('a').attr("title","text");
+
+// 14
+// $('a').attr("title","title");
+
+// 15
+// $('a').attr("title","text");
+// $('a').attr("class","text");
+
+// 16,17
+// $('.parent').append($('.child'))
+// $('.parent').prepend($('.child'))
+
+// 18
+
+
+// 19
+
+
+// 20
+// $('.child').remove()
+
+
+// 21
+// $('.child').empty()
